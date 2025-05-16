0e7262dda75d43d77a35c53e873b8a6697d813bb
Jorge <jorge.callem@upb.edu.co>
2025-05-16 12:17:57 -0500

    Nivel 2 y 3

---
+++ scripting2025-master/Assets/Scripts/Colision1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+
+public class Colision1 : MonoBehaviour
+{
+    
+    public void Creditos(){
+        MMSceneLoadingManager.LoadScene("Nivel3");
+    }
+}
